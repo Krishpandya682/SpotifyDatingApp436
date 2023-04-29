cf2aa6673abd3d950ecd96772d138c7682ee9f07
vrundal-shah <vrundalshah05@gmail.com>
2023-04-29 17:47:55 -0400

    minor-refactor move around getClosestUsers and calculate features functions

--- GroupApp436/RecommendationSystem.swift
+++ GroupApp436/RecommendationSystem.swift
@@ -1,12 +1,15 @@
 //
-//  ClosestUsers.swift
+//  RecommendationSystem.swift
 //  GroupApp436
 //
-//  Created by Krish Pandya on 4/25/23.
+//  Created by Vrundal Shah on 4/29/23.
 //
 
 import Foundation
+
 //Return an array of 5/10 closest users
 func getClosestUsers(spotifyUserId:String) -> [String] {
     return ["",""]
 }
+
+
--- GroupApp436/User.swift
+++ GroupApp436/User.swift
@@ -66,3 +66,7 @@ func getUsers(_ uid:String){
     print("HIIIII")
     FirestoreManager().getUsers(uid: uid)
 }
+
+func calculateFeatures(spotifyUserId: String) -> Features {
+    return Features(acousticness: 0, danceability: 0, duration_ms: 0, energy: 0, instrumentalness: 0, key: 0, liveness: 0, loudness: 0, mode: 0, speechiness: 0, tempo: 0, time_signature: 0)
+}
